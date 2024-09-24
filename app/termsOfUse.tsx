c1247c1381083aef1fb9baafe7ed1ecd734c630b
Arielcito <serato.arieli@gmail.com>
2024-09-23 21:21:08 -0300

    fix dependencies

--- app/termsOfUse.tsx
+++ app/termsOfUse.tsx
@@ -11,19 +11,19 @@ const TermsOfUse: React.FC = () => {
             <div>
                 <h1>Políticas y Términos de Uso</h1>
                 <h2>1. Introducción</h2>
-                <p>Bienvenido a [Nombre de la Aplicación]. Esta página describe nuestras políticas y términos de uso.</p>
+                <p>Bienvenido a RCF. Esta página describe nuestras políticas y términos de uso.</p>
 
                 <h2>2. Información que recopilamos</h2>
                 <p>Recopilamos información sobre su ubicación y acceso a la cámara para mejorar su experiencia.</p>
 
                 <h2>3. Uso de la cámara</h2>
-                <p>La cámara se utiliza para [describir el uso]. Se requiere su consentimiento para acceder a esta función.</p>
+                <p>La cámara se utiliza para obtener una foto de perfil del usuario. Se requiere su consentimiento para acceder a esta función.</p>
 
                 <h2>4. Uso de la ubicación</h2>
-                <p>Utilizamos su ubicación para [describir el uso]. Puede optar por no compartir su ubicación.</p>
+                <p>Utilizamos su ubicación para obtener la dirección de su domicilio y recomendarle canchas de fútbol cerca de su ubicación. Puede optar por no compartir su ubicación.</p>
 
                 <h2>5. Compartición de información</h2>
-                <p>No compartimos su información personal con terceros, excepto [describir excepciones].</p>
+                <p>No compartimos su información personal con terceros, excepto para cumplir con la ley.</p>
 
                 <h2>6. Seguridad de la información</h2>
                 <p>Implementamos medidas de seguridad para proteger su información.</p>
@@ -35,7 +35,7 @@ const TermsOfUse: React.FC = () => {
                 <p>Cualquier cambio en nuestras políticas será notificado a través de la aplicación.</p>
 
                 <h2>9. Contacto</h2>
-                <p>Para preguntas, contáctenos en [correo electrónico].</p>
+                <p>Para preguntas, contáctenos en serato.arieli@gmail.com</p>
             </div>
         </>
     );
